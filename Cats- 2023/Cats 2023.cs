17cc7570562e7df8667b005f193510eff5de6b45
Sok Eam <112976754+SokEam1@users.noreply.github.com>
2023-03-14 13:20:04 -0400

    Add files via upload

---
+++ Cats- 2023/Cats 2023.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cats__2023
+{
+    public Cat_2023(string name, string furColor, int age, int lives)
+    {
+        Name = name;
+        FurColor = furColor;
+        Age = Age;
+        Lives = lives;
+    }
+    private string name;
+    private string furColor;
+    private int age;
+    private int lives;
+
+    public string Name
+    {
+        get { return this.name; } //return the value
+        set { this.name = value; } //sets the value
+    }
+    public string FurColor
+    {
+        get { return this.furColor; }
+        set { this.furColor = value; }
+    }
+    public string Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+    public int Lives
+    {
+        get { return this.lives; }
+        set { this.lives = value; }
+    }
+
+}
+}
+    {
+    }
+}
